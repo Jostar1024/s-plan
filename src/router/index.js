abce63a80db6fba384e0ab8a013f2065a89a78a5
caoych <howard.eureka@gmail.com>
2018-04-27 15:59:05 +0800

    Add personal center component

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import index from '@/components/index'
 import login from '@/components/login'
 import register from '@/components/register'
+import personalcenter from '@/components/personalcenter'
 
 Vue.use(Router)
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: register
+    },
+    {
+      path: '/personalcenter',
+      name: 'personalcenter',
+      component: personalcenter
     }
   ]
 })
